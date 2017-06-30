42ff1ffab76055f0c9953d416fa1f5ee5b337738
Rashad Laher <rashadlaher@gmail.com>
2017-06-30 19:53:04 +0200

    first commit, print board working

---
+++ game.go
@@ -0,0 +1,93 @@
+package main
+
+import "fmt"
+
+const (
+	BOARDHEIGHT = 6
+	BOARDWIDTH  = 7
+)
+
+type Game struct {
+	GameBoard  [BOARDHEIGHT][BOARDWIDTH]string //check these are in the right order
+	heights    [BOARDWIDTH]int
+	NumMoves   int
+	IsComplete bool
+	LastMove   []int //x pos, y pos
+	LastPlayer string
+}
+
+// func (game *Game) MakeMove(col int) error {
+// 	if game.GameBoard[col] { //check if column is full -> make a heights array
+// 		return errors.New("Invalid Move: Column is full.")
+// 	}
+// 	game.GameBoard[col].append(true)
+// 	return nil
+// }
+
+func (game *Game) PrintBoard() {
+	// for a := BOARDHEIGHT; a > 0; a-- {
+	// 	for i := 0; i < BOARDWIDTH; i++ {
+	// 		fmt.Print("| ")
+	// 		fmt.Print(game.GameBoard[i][BOARDHEIGHT-1])
+	// 		fmt.Print(" |")
+	// 	}
+	// 	fmt.Println()
+	// }
+	for a := BOARDHEIGHT - 1; a >= 0; a-- {
+		for i := 0; i < BOARDWIDTH; i++ {
+			fmt.Print("| ")
+			if game.GameBoard[a][i] == "" {
+				fmt.Print(" ")
+			} else {
+				fmt.Print(game.GameBoard[a][i])
+			}
+			fmt.Print(" |")
+
+		}
+		fmt.Println()
+	}
+	//fmt.Print(game.GameBoard)
+}
+func (game *Game) CheckWinner() bool {
+	player := game.LastPlayer
+	xpos := game.LastMove[0]
+	ypos := game.LastMove[1]
+
+	numToWin := 3
+
+	//check horiz
+	//check to the left
+	for i := 1; xpos-i >= 0; i++ {
+		if game.GameBoard[xpos-i][ypos] == player {
+			numToWin--
+		} else {
+			break
+		}
+	}
+	//check to the right
+	for j := 1; xpos+j <= BOARDWIDTH-1; j++ {
+		if game.GameBoard[xpos+j][ypos] == player {
+			numToWin--
+		} else {
+			break
+		}
+	}
+	if numToWin <= 0 {
+		return true
+	}
+	return false
+	//check vertical
+
+	//diagnol
+}
+
+func main() {
+	var mygame Game
+	mygame.GameBoard[0][0] = "X"
+	mygame.GameBoard[0][1] = "O"
+	mygame.PrintBoard()
+	mygame.LastPlayer = "X"
+	mygame.LastMove = []int{2, 0}
+	a := mygame.CheckWinner()
+	fmt.Print(a)
+}
