4c4a06c83cd1d4839be0bd7d53f61a79fb4113b9
Rashad Laher <rashadlaher@gmail.com>
2017-07-04 14:50:29 +0200

    linked homepage to backend

--- main.go
+++ main.go
@@ -1,74 +1,19 @@
 package main
 
 import (
-	"connect4/game"
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	// var mygame game.Game
-	// player1 := player.Player{
-	// 	Name:   "Bob",
-	// 	Marker: "O",
-	// 	Game:   &mygame,
-	// }
-	// player2 := player.Player{
-	// 	Name:   "Joe",
-	// 	Marker: "X",
-	// 	Game:   &mygame,
-	// }
-	// for !mygame.IsComplete {
-	// 	//player one gets to go
-	// 	if !mygame.IsComplete {
-	// 		err := player1.MakeMove()
-	// 		for err != nil {
-	// 			fmt.Println(err.Error())
-	// 			err = player1.MakeMove()
-	// 		}
-	// 		mygame.IsComplete = mygame.CheckWinner()
-	// 	}
-	// 	//player2 gets to go
-	// 	if !mygame.IsComplete {
-	// 		err := player2.MakeMove()
-	// 		for err != nil {
-	// 			fmt.Println(err.Error())
-	// 			err = player2.MakeMove()
-	// 		}
-	// 		mygame.IsComplete = mygame.CheckWinner()
-	// 	}
-	// }
-	// // fmt.Println("Game over!")
-	// gameRoom := NewGameRoom()
-	// go gameRoom.run()
-	// http.HandleFunc("/", HomeHandler)
-	// http.HandleFunc("/room", func(w http.ResponseWriter, r *http.Request) {
-	// 	ServeWs(gameRoom, w, r)
-	// })
-	// fmt.Println("Starting server on :8080")
-	// http.ListenAndServe(":8080", nil)
+	//preparing mux and server
+	router := http.NewServeMux()
+	router.Handle("/", http.FileServer(http.Dir("./c4-react/public/")))
+	//router.Handle("/ws", wsHandler{})
 
-	mygame := game.NewGame()
-	mygame.AddPlayer()
-	mygame.AddPlayer()
-	mygame.MakeMove(0, 0)
-	mygame.MakeMove(1, 0)
-	fmt.Print(mygame.StringBoard())
-	mygame.MakeMove(1, 0)
-	mygame.MakeMove(0, 1)
-	fmt.Print(mygame.StringBoard())
-	mygame.MakeMove(1, 0)
-	mygame.MakeMove(0, 2)
-	fmt.Print(mygame.StringBoard())
-	mygame.MakeMove(1, 0)
-	mygame.MakeMove(0, 3)
-	fmt.Print(mygame.StringBoard())
-	if !mygame.IsComplete {
-		mygame.MakeMove(0, 4)
-		fmt.Print(mygame.IsComplete)
-		fmt.Print(mygame.StringBoard())
-	}
+	//serving
+	log.Printf("serving connect 4 live on localhost: 8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
 
 }
 
